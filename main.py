bf1c02071b416963c91ce2df2c933550fc03f03b
Abtin <abtin.afshar@gmail.com>
2023-06-19 23:54:54 -0400

    multi-pdf support

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-from loguru import logger as log
-from dotenv import load_dotenv
-import streamlit as st
-from streamlit_chat import message
+import base64
+
+from ui_templates import css_style, ai_template, user_template
+
 import os
+
+import streamlit as st
+from loguru import logger as log
 from PyPDF2 import PdfReader
-from langchain.text_splitter import CharacterTextSplitter
+from dotenv import load_dotenv
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.memory import ConversationBufferMemory
+from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import Chroma
-from langchain.chains.question_answering import load_qa_chain
-from langchain.llms import OpenAI
-from langchain.schema import (
-    SystemMessage,
-    HumanMessage,
-    AIMessage
-)
 
 
 def init():
@@ -22,54 +22,90 @@ def init():
     if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
         log.error("OPENAI_API_KEY is not set")
         exit(1)
-    st.set_page_config("PDF Whisperer", page_icon="💬", layout="wide")
+    st.set_page_config("Market Whisperer", page_icon="💬", layout="wide")
+    st.write(css_style, unsafe_allow_html=True)
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+
+def load_images(user_image_file, robot_image_file):
+    user_image = open(user_image_file, "rb")
+    robot_image = open(robot_image_file, "rb")
+    user_url = base64.b64encode(user_image.read()).decode()
+    robot_url = base64.b64encode(robot_image.read()).decode()
+    user_image.close()
+    robot_image.close()
+    return user_url, robot_url
+
+
+def extract_text_from_pdf(pdf_file) -> str:
+    text = ""
+    for pdf in pdf_file:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+
+def create_chunks(text: str) -> list[str]:
+    text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+
+def create_vector_db(chunks: list[str]) -> Chroma:
+    return Chroma.from_texts(texts=chunks, embedding=OpenAIEmbeddings(), persist_directory="./embeddings")
+
+
+def create_conversation_chain(vectordb: Chroma):
+    buffered_memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    chain = ConversationalRetrievalChain.from_llm(
+        llm=ChatOpenAI(),
+        retriever=vectordb.as_retriever(),
+        memory=buffered_memory
+    )
+    return chain
+
+
+def generate_answer(query: str):
+    response = st.session_state.conversation({"question": query})
+    st.session_state.chat_history = response["chat_history"]
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template
+                     .replace("{USER_MSG}", message.content)
+                     .replace("{USER_IMAGE}", st.session_state.user_image),
+                     unsafe_allow_html=True)
+        else:
+            st.write(ai_template
+                     .replace("{AI_MSG}", message.content)
+                     .replace("{ROBOT_IMAGE}", st.session_state.robot_image),
+                     unsafe_allow_html=True)
 
 
 def main():
     init()
-    st.title("PDF Whisperer 💬")
+    st.session_state.user_image, st.session_state.robot_image = load_images("static/person.png", "static/robot.png")
+    st.title("Market Whisperer 💬")
+    query = st.text_input("Enter your question here:")
+    if query != "":
+        generate_answer(query)
 
-    pdf = st.file_uploader("Upload your PDF file", type=["pdf"])
-    if pdf is not None:
-        log.info("PDF file uploaded")
-        with st.spinner("Thinking..."):
-            pdf_reader = PdfReader(pdf)
-            text = ""
-            for page in pdf_reader.pages:
-                text += page.extract_text()
-
-            text_splitter = CharacterTextSplitter(
-                separator="\n",
-                chunk_size=1000,
-                chunk_overlap=100,
-                length_function=len)
-            chunks = text_splitter.split_text(text)
-
-            embeddings = OpenAIEmbeddings()
-            knowledge_base = Chroma.from_texts(texts=chunks, embedding=embeddings)
-
-        messages = st.session_state.get("messages", [])
-        if "messages" not in st.session_state:
-            st.session_state.messages = [
-                SystemMessage(content="You only answer questions about the pdf here"),
-            ]
-
-        user_question = st.text_input("Ask a question about your pdf:", key="user_input")
-
-        if user_question:
-            st.session_state.messages.append(HumanMessage(content=user_question))
-            with st.spinner("Preparing ..."):
-                docs = knowledge_base.similarity_search(user_question, k=3)
-                llm = OpenAI()
-                chain = load_qa_chain(llm=llm, chain_type="stuff")
-                response = chain.run(input_documents=docs, question=user_question)
-                st.session_state.messages.append(AIMessage(content=response))
-
-            for i, msg in enumerate(messages[1:]):
-                if i % 2 == 0:
-                    message(msg.content, is_user=True, key=str(i) + "_user", avatar_style="croodles")
-                else:
-                    message(msg.content, is_user=False, key=str(i) + "_ai", avatar_style="bottts")
+    with st.sidebar:
+        st.subheader("Annual Reports")
+        pdf_files = st.file_uploader("Upload annual reports:", accept_multiple_files=True, type=["pdf"])
+        if st.button("Submit"):
+            with st.spinner("Processing..."):
+                text = extract_text_from_pdf(pdf_files)
+                chunks = create_chunks(text)
+                vector_db = create_vector_db(chunks)
+                st.session_state.conversation = create_conversation_chain(vector_db)
 
 
 if __name__ == '__main__':
